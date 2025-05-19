cd1f46dd9426073e0a0f70dc5127558cb94bcc3e
hamzaadnan03 <hamza.adnan@thewebhunters.com>
2025-05-19 18:24:25 +0500

    add config setting

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("server is running");
+import app from "./src/app";
+import { config } from "./src/config/config";
+
+const startServer = () => {
+  const port = config.port || 3000;
+  app.listen(port, () => {
+    console.log(`server is listening on port: ${port}`);
+  });
+};
+startServer();
